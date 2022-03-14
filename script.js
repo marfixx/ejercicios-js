fbfa4172b547c4384453aaabc3f1ad932a0392db
Mayra <ada_hack242@hackademy.mx>
2022-03-13 20:27:06 -0600

    Solución a los ejercicios

--- script.js
+++ script.js
@@ -35,3 +35,58 @@ const ejercicio2 = [
     esFamiliar: true,
   },
 ];
+
+//Solucion al ejercicio 1
+let primos="Respuesta: ";
+
+ejercicio1.forEach(element => {
+  if(isPrime(element)){
+  primos+=element+', ';
+  }
+});
+
+document.getElementById('uno').innerHTML=primos.slice(0,-2)+'.';
+
+function isPrime(number) {
+  var times=0;
+  for(var i=1;i<=number;i++){
+    if(Number.isInteger(number/i)){
+      times++;
+      if(times>2){
+        return false;
+      }
+    }
+  }
+
+  if(times==2){
+    return true;
+  }else{
+    return false;
+  }
+
+
+}
+
+//Solución al ejercicio 2
+var usuarios="Pasan: ";
+ejercicio2.forEach(element=>{
+if(element.edad>=18&&element.esFamiliar){
+  usuarios+=element.nombre+", ";
+}
+});
+
+document.getElementById('dos').innerText=usuarios.slice(0,-2)+'.';
+
+//Solucion al ejercicio3
+var semilla=1;
+var sucesor=1;
+var fibo="1(1), 1(2), ";
+for(var i=1;i<49;i++){
+  aux=sucesor;
+  sucesor+=semilla;
+  semilla=aux;
+  fibo+=sucesor+"("+(i+2)+"), ";
+
+}
+
+document.getElementById('tres').innerText=fibo.slice(0,-2)+'.';
